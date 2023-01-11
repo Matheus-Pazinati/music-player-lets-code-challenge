6b7cd44cdbc0b435786efc7fbd9da69b8fa1d6ca
Matheus Pazinati <matheuspazinati@gmail.com>
2023-01-11 05:23:46 -0300

    Corrige posicionamento e altera o src das imagens

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,27 +1,25 @@
 import * as Progress from '@radix-ui/react-progress';
 
-import musicImg from '../../assets/images/Florian.jpg'
-
 import { Play, FastForward, Rewind } from 'phosphor-react'
 
 export function Player() {
   const progress = 50
   return (
-    <main className='flex justify-center items-center h-[100vh]'>
+    <main className='flex justify-center items-start sm:items-center h-[100vh]'>
       <section
-        className='bg-[#2A2141] mx-4 rounded-md max-w-xs'
+        className='bg-[#2A2141] mx-4 my-4 rounded-md max-w-xs sm:w-[500px] sm:max-w-none'
       >
         <div
-          className='px-9 py-14' >
-          <div className='flex flex-col gap-7 '>
-            <img src={musicImg} alt="" className='rounded-md' />
+          className='px-9 sm:px-7 py-14 sm:py-7' >
+          <div className='flex flex-col sm:flex-row gap-7 sm:items-center sm:gap-9 '>
+            <img src="https://phx02pap001files.storage.live.com/y4m14tKYTSDdKN4DQnuXHjsPTs5BFv1LgUFGJtP5OVRDqd8hwDlWPvYJ5uQEGEg9b_ZbLy0GPB1OY-4ulU8SxP9Iyo0ke4nwEZNdxJQEcepAbGr6PbqKt_0is2nzdLVwbBsV7u9v6Z2CmEWauj_9CGAC_1TiNJEoKbpnwOhyXAW8ZP6KW3RgJq-ZyFt50Zt4ZBO?width=640&height=640&cropmode=none" alt="" className='rounded-md sm:w-[150px] sm:h-[150px]' />
             <div>
               <h2 className='text-[#E1E1E6] font-bold text-2xl'>Janking Roe</h2>
               <p className='text-[#E1E1E6] text-lg'>Florian</p>
             </div>
           </div>
           <div className='w-full'>
-            <div className='flex justify-between items-center w-full my-7'>
+            <div className='flex justify-between sm:justify-around items-center w-full my-7'>
               <button>
                 <Rewind size={28} weight={'fill'} color={'#E1E1E6'} />
               </button>
@@ -35,7 +33,7 @@ export function Player() {
             <div>
               <Progress.Root
                 value={50}
-                className='w-full h-2 bg-[#0B0B0B] rounded-full relative overflow-hidden'
+                className='w-full h-2 bg-[#1B1B1B] rounded-full relative overflow-hidden'
               >
                 <Progress.Indicator
                   className='w-full h-full bg-[#D9D9D9] rounded-full transition trasform duration-1000 ease-[cubic-bezier(0.65, 0, 0.35, 1)]'
